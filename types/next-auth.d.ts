81f1618deaef1d0d81b4ca28cd14cb18af467b18
selwin saputra <selwins94@gmail.com>
2025-11-19 18:47:33 +0800

    tambahan2

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,19 +1,26 @@
-import NextAuth from "next-auth"
+import NextAuth, { DefaultSession } from "next-auth"
 
 declare module "next-auth" {
   interface Session {
     user: {
-      id: number
+      id: int
       name: string
-      email: string
+      email?: string
       role: string
-    }
+    } & DefaultSession["user"]
   }
 
   interface User {
-    id: number
+    id: int
     name: string
-    email: string
+    role: string
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: int
+    name: string
     role: string
   }
 }
